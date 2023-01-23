6499ee86aa97931dd0b60f27f3e88dc2d4d93801
Anna Romashko <rexdouble9@gmail.com>
2023-01-23 17:41:14 +0300

    add user voter and user password hasher

---
+++ src/Security/Voter/UserVoter.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserVoter extends Voter
+{
+    private $security = null;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    protected function supports($attribute, $subject): bool
+    {
+        $supportsAttribute = in_array($attribute, ['USER_EDIT']);
+        $supportsSubject = $subject instanceof User;
+
+        return $supportsAttribute && $supportsSubject;
+    }
+
+    /**
+     * @param string $attribute
+     * @param User $subject
+     * @param TokenInterface $token
+     * @return bool
+     */
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token): bool
+    {
+        if ($attribute == 'USER_EDIT'){
+                $user = $this->security->getUser();
+                if ($user == $subject or $this->security->isGranted('ROLE_ADMIN')) {return true;}
+        }
+        return false;
+    }
+}
